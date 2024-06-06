d4c53928e18ede5d1a7231a3518417a9f34ac96c
Arela-Cristina <instagrandine@gmail.com>
2024-06-06 02:13:13 +0200

    Add const serFullNameInput
    Add const
    numberKmInput
    
    Add const
    passengerAgeInput
    
    Add cont generateButton and gave and object .addEventListener "click". And then gave, the last one a function to make appear the
    name
    offer
    carriage
    and price
    under the respectives conditions.

---
+++ main.js
@@ -0,0 +1,56 @@
+const userFullNameInput = document.querySelector(".userFullNameInput");
+
+
+const numberKmInput = document.querySelector(".travelKm");
+
+
+const passengerAgeInput = document.querySelector(".age");
+
+
+const generateButton = document.querySelector(".generate");
+generateButton.addEventListener("click",
+    function () {
+        let userFullNameValue = userFullNameInput.value;
+        console.log(userFullNameValue);
+        document.querySelector(".userFullName").innerHTML = userFullNameValue;
+
+        let offer = "Standard Price"
+        let priceKm = 0.21;
+        let numberKm = numberKmInput.value;
+        let travelPrice = numberKm * priceKm;
+        let passengerAge = passengerAgeInput.value;
+
+        if (passengerAge < 18) {
+            let percentage = (travelPrice * 20) / 100;
+            travelPrice = (travelPrice - percentage).toFixed(2);
+            offer = "Discount Price";
+            console.log("prezzo scontato per minoreni", travelPrice + '€');
+
+
+        } else if (passengerAge < 65) {
+            travelPrice = numberKm * priceKm;
+            console.log("prezzo senza nessun tipo di sconto, intero", travelPrice + '€');
+
+        } else {
+            let percentage = (travelPrice * 40) / 100;
+            travelPrice = (travelPrice - percentage).toFixed(2);
+            offer = "Discount Price";
+            console.log("prezzo scontato per over 65", travelPrice + '€');
+
+        }
+
+        document.querySelector(".offer").innerHTML = offer;
+
+        let carriage = Math.floor(Math.random() * 10);
+        document.querySelector(".carriage").innerHTML = carriage;
+
+        let cp = Math.floor(Math.random() * 9999);
+        document.querySelector(".cp").innerHTML = cp;
+
+        document.querySelector(".cost").innerHTML = travelPrice + '€';
+
+    }
+);
+
+
+
